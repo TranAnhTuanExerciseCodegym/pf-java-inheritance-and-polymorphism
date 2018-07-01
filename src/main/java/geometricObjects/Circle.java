001179b3292fb5deec77ad9cd7f308a62f284a6c
Anh Tuan <tincanxh@gmail.com>
2018-07-01 17:51:17 +0700

    add gitignore

---
+++ src/main/java/geometricObjects/Circle.java
@@ -0,0 +1,18 @@
+package geometricObjects;
+
+public class Circle extends Shape {
+    double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+}
